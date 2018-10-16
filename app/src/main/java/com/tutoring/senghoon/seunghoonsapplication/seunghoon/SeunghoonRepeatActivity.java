d33057eeee634a587683d03dcbf937a4b2c8ed76
SeunghoonShin <shshin06@daum.net>
2018-10-16 20:23:02 +0900

    Modify SeunghoonRepeatActivity

--- app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/seunghoon/SeunghoonRepeatActivity.java
+++ app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/seunghoon/SeunghoonRepeatActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import com.tutoring.senghoon.seunghoonsapplication.R;
 
+import java.util.FormatFlagsConversionMismatchException;
+
 public class SeunghoonRepeatActivity extends AppCompatActivity {
     LinearLayout llBoolean1;
     LinearLayout llBoolean2;
@@ -19,7 +21,8 @@ public class SeunghoonRepeatActivity extends AppCompatActivity {
     Button btnBooleanResult2;
     TextView tvBooleanResult2;
 
-
+        TextView tvForNumber;
+        TextView tvForNumber2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +37,78 @@ public class SeunghoonRepeatActivity extends AppCompatActivity {
         tvBooleanTitle2 = findViewById(R.id.sh_repeat_tvBooleanTitle2);
         btnBooleanResult2 = findViewById(R.id.sh_repeat_btnBoolean2Result);
         tvBooleanResult2 = findViewById(R.id.sh_repeat_tvBoolean2Result);
+
+        tvForNumber = findViewById(R.id.sh_repeat_tvForNumber);
+        tvForNumber2 = findViewById(R.id.sh_repeat_tvForNumber2);
+        // for 문
+        // for (변수초기화;조건식; 변수 변경) {
+        // 수행
+        //}
+
+        String stringNumber = "";
+        for (int i = 1; i < 10; i++) {
+          stringNumber = stringNumber + i;
+          tvForNumber.setText(stringNumber);
+
+        }
+        int sum = 0;
+        for (int j = 0;j < 10; j++) {
+
+            sum = sum + j;
+            tvForNumber.setText(String.valueOf(sum));
+
+        }
+        String stars1 = "";
+        for (int x = 0; x < 5; x++) {
+
+             stars1 = stars1 + "짝";
+
+             for (int y = 0; y <= x; y++) {
+
+                stars1 = stars1 + "뿡";
+
+//                stars1 = stars1 + "*";
+            }
+
+
+            stars1 = stars1 + "\n";
+
+        }
+        tvForNumber.setText(stars1);
+
+
+
+
+
+
+
+
+
+        String number1 = "";
+
+        for (int x = 1; x <= 5; x--) { //세로우
+
+            number1 = number1 + "|";
+
+            for (int y = 1; y <= 3; y--) {  //가로우
+                number1 = number1 + y;
+            }
+
+            number1 = number1 + x + "\n";
+        }
+        tvForNumber2.setText(number1);
+
+        String number2 = "";
+
+        for (int x = 5; x >=1; x--){
+
+            for (int y = x; y >= 1; y--) {
+                number2 = number2 + "*";
+            }
+
+            number2 = number2 + "\n";
+        }
+
+        tvForNumber2.setText(number2);
     }
 }
