3fff4171a0fe436a1277d488553dcf02e6fdb5dc
Hyelim Kim <hayley7927@gmail.com>
2018-10-16 20:12:36 +0900

    Modify HayleyRepeatActivity.java

--- app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/hayley/HayleyRepeatActivity.java
+++ app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/hayley/HayleyRepeatActivity.java
@@ -20,6 +20,8 @@ public class HayleyRepeatActivity extends AppCompatActivity {
     Button btnBooleanResult2;
     TextView tvBooleanResult2;
 
+    TextView tvForNumber;
+    TextView tvForNumber2;
 
 
     @Override
@@ -37,7 +39,81 @@ public class HayleyRepeatActivity extends AppCompatActivity {
         btnBooleanResult2 = findViewById(R.id.hl_repeat_btnBoolean2Result);
         tvBooleanResult2 = findViewById(R.id.hl_repeat_tvBoolean2Result);
 
+        tvForNumber = findViewById(R.id.hl_repeat_tvForNumber);
+        tvForNumber2 = findViewById(R.id.hl_repeat_tvForNumber2);
 
+
+        // for 문
+        // for (변수 초기화; 조건식; 변수 변경) {
+        //   ... 수행
+        // }
+
+        String stringNumber = "";
+
+        for (int i = 1; i < 10; i++) {
+
+            stringNumber = stringNumber + i;
+
+            tvForNumber.setText(stringNumber);
+
+        }
+
+        int sum = 0;
+
+        for (int j = 0; j < 10; j++) {
+
+            sum = sum + j;
+
+            tvForNumber.setText(String.valueOf(sum));
+        }
+
+
+        String stars1 = "";
+
+        for (int x = 0; x < 5; x++) {
+
+            for (int y = 0; y < 1; y++) {
+                stars1 = stars1 + "*";
+            }
+
+            stars1 = stars1 + "\n";
+        }
+
+        tvForNumber.setText(stars1);
+
+        // for (초기화; 조건식; 증감식) {
+
+        String number1 = "";
+
+        for (int x = 5; x >= 1; x--) {
+
+            for (int y = 3; y >= 1; y--) {
+                number1 = number1 + y;
+            }
+
+            number1 = number1 + x + "\n";
+        }
+
+        tvForNumber2.setText(number1);
+
+
+        tvForNumber2.setText(number1);
+
+
+        String number2 = "";
+
+        for (int x = 5; x >= 1; x--) {
+
+
+            for (int y = x; y >= 1; y--) {
+                number2 = number2 + "*";
+            }
+
+            number2 = number2 + "\n";
+        }
+
+        tvForNumber2.setText(number2);
 
     }
+
 }
