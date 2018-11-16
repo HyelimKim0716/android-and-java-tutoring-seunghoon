e0c6ec788a83653f21de6764f9e7b47f3df35f30
SeunghoonShin <shshin06@daum.net>
2018-11-16 20:14:42 +0900

    Add SeunghoonMethod2Activity xml file

--- app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/seunghoon/SeunghoonMethod2Activity.java
+++ app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/seunghoon/SeunghoonMethod2Activity.java
@@ -2,14 +2,18 @@ package com.tutoring.senghoon.seunghoonsapplication.seunghoon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.tutoring.senghoon.seunghoonsapplication.R;
 
 public class SeunghoonMethod2Activity extends AppCompatActivity {
 
+    TextView tvHanResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seunghoon_method2);
+
+        tvHanResult = findViewById(R.id.sh_method2_tvHanResult);
     }
 }
