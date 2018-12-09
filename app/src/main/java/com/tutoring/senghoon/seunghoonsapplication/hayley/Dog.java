4909dff83f5921039bb78341b3e5eec7f8e38250
hyelimkim <hayley7927@gmail.com>
2018-12-09 15:08:26 +0900

    Modify HayleyDogClassActivity

---
+++ app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/hayley/Dog.java
@@ -0,0 +1,31 @@
+package com.tutoring.senghoon.seunghoonsapplication.hayley;
+
+public class Dog {
+
+    // 속성
+    String breed;
+
+    String size;
+
+    int age;
+
+    String color;
+
+    // 생성자 : 클래스 생성시 해야할 일을 구현
+    Dog(String breed, String size, int age, String color) {
+        this.breed = breed;
+        this.size = size;
+        this.age = age;
+        this.color = color;
+    }
+
+
+
+    // 함수
+
+    void eat(String value1, String value2) {
+
+
+    }
+
+}
--- app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/hayley/HayleyDogClassActivity.java
+++ app/src/main/java/com/tutoring/senghoon/seunghoonsapplication/hayley/HayleyDogClassActivity.java
@@ -2,14 +2,41 @@ package com.tutoring.senghoon.seunghoonsapplication.hayley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.tutoring.senghoon.seunghoonsapplication.R;
 
 public class HayleyDogClassActivity extends AppCompatActivity {
 
+    TextView tvBreed1, tvSize1, tvAge1, tvColor1;
+    TextView tvBreed2, tvSize2, tvAge2, tvColor2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hayley_dog);
+        setContentView(R.layout.activity_hayley_dog_class);
+
+        tvBreed1 = findViewById(R.id.hl_dogClass_tvBreed1);
+        tvSize1 = findViewById(R.id.hl_dogClass_tvSize1);
+        tvAge1 = findViewById(R.id.hl_dogClass_tvAge1);
+        tvColor1 = findViewById(R.id.hl_dogClass_tvColor1);
+
+        tvBreed2 = findViewById(R.id.hl_dogClass_tvBreed2);
+        tvSize2 = findViewById(R.id.hl_dogClass_tvSize2);
+        tvAge2 = findViewById(R.id.hl_dogClass_tvAge2);
+        tvColor2 = findViewById(R.id.hl_dogClass_tvColor2);
+
+
+        Dog seunghoonDog = new Dog("비숑", "Medium", 2, "White");
+        Dog teacherDog = new Dog("포메라니안", "Small", 10, "Brown");
+
+
+        tvBreed1.setText(seunghoonDog.breed);
+        tvSize1.setText(seunghoonDog.size);
+        tvAge1.setText(String.valueOf(seunghoonDog.age));
+        tvColor1.setText(seunghoonDog.color);
+
+        
+
     }
 }
